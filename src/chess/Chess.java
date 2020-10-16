08e58452361f1cb45f87bdd310e388e42d4141c2
UJJAVAL SHAH <us89@scarletmail.rutgers.edu>
2020-10-16 17:49:39 +0000

    Chess.java created chessBoard 2D array

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -3,9 +3,26 @@ package chess;
 public class Chess
 {
 
+	public static String[][] chessBoard;
+
 	public static void main(String[] args)
 	{
 		System.out.println("qwerty");
+		
+		
+		chessBoard[0][8]="8";
+		chessBoard[1][8]="7";
+		chessBoard[2][8]="6";
+		chessBoard[3][8]="5";
+		chessBoard[4][8]="4";
+		chessBoard[5][8]="3";
+		chessBoard[6][8]="2";
+		chessBoard[7][8]="1";
+		chessBoard[8][8]=null;
+		
+		
+		
+		
 	}
 
 }
