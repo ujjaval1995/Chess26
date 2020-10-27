cfc90b7cb6edc4263b61591ce72aecf2c452d06f
Jishnu <jishnupatel1@gmail.com>
2020-10-26 23:38:37 -0400

    modified Piece

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -77,39 +77,94 @@ public abstract class Piece
 		{
 			int min = Math.min(col1, col2);
 			int max = Math.max(col1, col2);
-			for (int i=min+1; i<max; i++)
+			for (int i=min+1; i<max; i++) // clear path
 			{
 				if (Board.board[row1][i] != null)
 				{
 					return false;
 				}
 			}
-			Piece piece = Board.board[row2][col2];
-			Board.board[row1][col1] = null;
-			Board.board[row2][col2] = this;
-			if (Board.check(color))
+			if (Board.board[row2][col2] == null) // move
 			{
-				Board.board[row1][col1] = this;
-				Board.board[row2][col2] = null;
-				return false;
+				Board.board[row1][col1] = null;
+				Board.board[row2][col2] = this;
+				if (Board.check(color))
+				{
+					Board.board[row1][col1] = this;
+					Board.board[row2][col2] = null;
+					return false;
+				}
+				else
+				{
+					return true;
+				}
 			}
-			else
+			else if (!Board.board[row2][col2].hasColor(color)) // capture
 			{
-				return true;
+				Piece piece = Board.board[row2][col2];
+				Board.board[row1][col1] = null;
+				Board.board[row2][col2] = this;
+				if (Board.check(color))
+				{
+					Board.board[row1][col1] = this;
+					Board.board[row2][col2] = piece;
+					return false;
+				}
+				else
+				{
+					return true;
+				}
 			}
+			
 		}
 		else if (row1 != row2 && col1 == col2) // vertical
 		{
 			int min = Math.min(row1, row2);
 			int max = Math.max(row1, row2);
-			for (int i=min+1; i<max; i++)
+			for (int i=min+1; i<max; i++) // clear path
 			{
 				if (Board.board[i][col1] != null)
 				{
 					return false;
 				}
 			}
+			if (Board.board[row2][col2] == null) // move
+			{
+				Board.board[row1][col1] = null;
+				Board.board[row2][col2] = this;
+				if (Board.check(color))
+				{
+					Board.board[row1][col1] = this;
+					Board.board[row2][col2] = null;
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+			}
+			else if (!Board.board[row2][col2].hasColor(color)) // capture
+			{
+				Piece piece = Board.board[row2][col2];
+				Board.board[row1][col1] = null;
+				Board.board[row2][col2] = this;
+				if (Board.check(color))
+				{
+					Board.board[row1][col1] = this;
+					Board.board[row2][col2] = piece;
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+			}
 		}
+		return false;
+	}
+	
+	public boolean move_diagonal(String input)
+	{
 		return false;
 	}
 }
