0ce7ecc4a0d212c6141f8c36365b49846d041376
Ujjaval Shah <us89@scarletmail.rutgers.edu>
2020-10-28 16:31:03 -0400

    added javadocs

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"chess","l":"Bishop"},{"p":"chess","l":"Board"},{"p":"chess","l":"Chess"},{"p":"chess","l":"King"},{"p":"chess","l":"Knight"},{"p":"chess","l":"Pawn"},{"p":"chess","l":"Piece"},{"p":"chess","l":"Queen"},{"p":"chess","l":"Rook"}]
